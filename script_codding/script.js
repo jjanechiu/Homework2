b18f0c6ed17c23843360a4a426aa08f1650179c4
jjanechiu <105808145+jjanechiu@users.noreply.github.com>
2025-08-16 16:30:48 +0800

    Add files via upload

---
+++ script_codding/script.js
@@ -0,0 +1,14 @@
+// // alert("hello world");
+// console.log("hello world");
+
+let age2 = 70;
+let isOrder = age2 > 65; // 請給我一個判斷式 Boolean, age2 大於 65 成立時為 true
+let isNotOrder = age2 < 65; // 請給我一個判斷式 Boolean, age2 不大於/等於 65 成立時為 true
+let is70Age = age2 === 70; // 請給我一個判斷式 Boolean, age2 等於 70 成立時為 true
+let impossibleStatement = isOrder && isNotOrder; // ??
+
+console.log("age2: ", age2);
+console.log("isOrder: ", isOrder);
+console.log("isNotOrder: ", isNotOrder);
+console.log("is70Age: ", is70Age);
+console.log("impossibleStatement: ", impossibleStatement);
